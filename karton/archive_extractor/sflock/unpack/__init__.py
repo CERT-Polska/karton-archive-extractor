fdef10d4120a96ef94f17934999f4a3b49ff089c
Paweł Srokosz <pawel.srokosz@cert.pl>
2021-07-08 15:49:49 +0200

    Use doomedraven's SFLock2 instead of embedded fork

--- karton/archive_extractor/sflock/unpack/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2015-2016 Jurriaan Bremer.
-# This file is part of SFlock - http://www.sflock.org/.
-# See the file 'docs/LICENSE.txt' for copying permission.
-
-from ..abstracts import Unpacker
-from ..misc import import_plugins
-
-plugins = import_plugins(__file__, __name__, globals(), Unpacker)
