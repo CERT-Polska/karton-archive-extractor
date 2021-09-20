3262ddcd7e3721b40b6a263f49cc4558c14793d5
Alex Ilgayev <alex.ilgayev@gmail.com>
2021-09-20 13:50:06 +0300

    Added a limitation for number of childs (#27)
    Co-authored-by: Alex Ilgayev <alexil@checkpoint.com>

--- karton/archive_extractor/archive_extractor.py
+++ karton/archive_extractor/archive_extractor.py
@@ -16,6 +16,8 @@ class ArchiveExtractor(Karton):
     max_depth = 5
     # Maximum unpacked child filesize, larger files are not reported
     max_size = 25 * 1024 * 1024
+    # Maximum number of childs for further analysis
+    max_children = 1000
 
     identity = "karton.archive-extractor"
     version = __version__
@@ -82,6 +84,10 @@ class ArchiveExtractor(Karton):
 
         if not unpacked.children:
             self.log.warning("Don't know how to unpack this archive")
+            return
+
+        if len(unpacked.children) > self.max_children:
+            self.log.warning("Too many children for further processing")
             return
 
         for child in unpacked.children:
