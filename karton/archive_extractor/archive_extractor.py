120e016d8153839877ac9d8f03d637b0af404ba5
msm-cert <156842376+msm-cert@users.noreply.github.com>
2025-07-28 11:19:08 +0000

    Update karton/archive_extractor/archive_extractor.py

--- karton/archive_extractor/archive_extractor.py
+++ karton/archive_extractor/archive_extractor.py
@@ -77,7 +77,7 @@ class ArchiveExtractor(Karton):
 
             process_pe(
                 pe,
-                cert_preservation=True,
+                cert_preservation=False,
                 out_path=temp_file.as_posix(),
                 last_ditch_processing=False,
                 log_message=log_message_wrapped,
